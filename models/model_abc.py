3023b28143f36f4f1682b51ed0f1c450da1e1d1c
Whillikers <aidanswope@gmail.com>
2019-09-03 01:34:50 -0700

    Skeleton of training code

--- models/model_abc.py
+++ models/model_abc.py
@@ -46,7 +46,7 @@ class EmbeddingModelABC(tf.keras.Model, ABC):
         card_ids = [self._encoding[name] for name in card_names]
         return self.call(card_ids)
 
-    def k_nearest_cards(self, cards, k=10):
+    def k_nearest_cards_and_similarities(self, cards, k=10):
         """
         For each card in cards, return the k card names that are most similar.
 
@@ -64,14 +64,21 @@ class EmbeddingModelABC(tf.keras.Model, ABC):
         -------
         [[str]]
             For each card, a list of other card ids most similar.
+        [[float]]
+            For each card, how similar each other card is.
         """
+        # TODO: solve tensor problem
+
         if type(cards[0]) == str:
             embeddings = self.card_embeddings(cards)
         else:
-            embeddings = tf.constant(cards)
+            embeddings = cards
 
-        similar_ids = self._k_nearest_ids_to_embeddings(embeddings, k)
-        return [[self._decoding(id_) for id_ in row] for row in similar_ids]
+        similar_ids, similarities = self._k_nearest_ids_to_embeddings(embeddings, k)
+        similar_cards = [
+            [self._decoding[id_] for id_ in row] for row in similar_ids.numpy()
+        ]
+        return similar_cards, similarities
 
     @abstractmethod
     def call(self, card_ids):
@@ -112,7 +119,7 @@ class EmbeddingModelABC(tf.keras.Model, ABC):
         raise NotImplementedError
 
     @abstractmethod
-    def _loss(self, batch):
+    def _loss(self, batch, step=None):
         """
         Compute the loss associated with a set of features.
 
@@ -121,6 +128,9 @@ class EmbeddingModelABC(tf.keras.Model, ABC):
         batch
             Some structured tf.Tensors, with exact structure depending on the
             dataset.
+        step (optional) : None or int
+            The step used for plotting summaries.
+            If None, do not make summaries.
 
         Returns
         -------
@@ -129,5 +139,75 @@ class EmbeddingModelABC(tf.keras.Model, ABC):
         """
         raise NotImplementedError
 
-    def train(self):
-        raise NotImplementedError  # TODO
+    def train(
+        self,
+        dataset,
+        learning_rate,
+        watch_cards=["Lightning Bolt", "Brainstorm", "Command Tower", "Thoughtseize"],
+    ):
+        """
+        TODO
+        """
+
+        optimizer = tf.optimizers.Adam(learning_rate, clipnorm=1)
+        writer = tf.summary.create_file_writer(self.out_dir)
+        watch_ids = tf.constant([self._encoding[name] for name in watch_cards])
+
+        # TODO: checkpoints
+        # TODO: embedding projector
+        with writer.as_default():
+            for step, batch in enumerate(dataset):
+                with tf.GradientTape() as tape:
+                    loss = self._loss(batch, step)
+
+                gradients = tape.gradient(loss, self.trainable_variables)
+                grads_and_vars = list(zip(gradients, self.trainable_variables))
+                for grad, var in grads_and_vars:
+                    tf.summary.histogram(
+                        "gradients/{}".format(var.name), grad.values, step
+                    )
+                optimizer.apply_gradients(grads_and_vars)
+
+                # For each of a few chosen cards, show a table of other similar cards
+                watch_embeddings = self.call(watch_ids)
+                similar_cards, similarities = self.k_nearest_cards_and_similarities(
+                    watch_embeddings
+                )
+                similar_card_table = self._format_similar_card_table(
+                    watch_cards, similar_cards, similarities
+                )
+                tf.summary.histogram("watch_embeddings", watch_embeddings, step)
+                tf.summary.text("watch_similar_cards", similar_card_table, step)
+
+    @staticmethod
+    def _format_similar_card_table(header_cards, similar_cards, similarities):
+        """
+        Format a table of cards and their neighbors in embedding space.
+
+        See: https://github.com/tensorflow/tensorboard/blob/master/
+             tensorboard/plugins/text/text_demo.py
+
+        Parameters
+        ----------
+        header_cards : [string]
+            The cards used as anchors.
+        similar_cards : [[string]]
+            For each anchor card, a list of similar cards.
+        similarties: [[float]]
+            For each card, how similar each other card is.
+
+        Returns
+        -------
+        string
+            A Markdown table of the input cards
+        """
+        header = "|".join(header_cards)
+        subheader = "---|---" * (len(header_cards) - 1)
+
+        sims_formatted = [
+            [f"{card} ({sim.numpy():.3f})" for card, sim in zip(*pair_list)]
+            for pair_list in zip(similar_cards, similarities)
+        ]
+        rows = ["|".join(list(row)) for row in zip(*sims_formatted)]
+        table = "{}\n{}\n{}".format(header, subheader, "\n".join(rows))
+        return table
