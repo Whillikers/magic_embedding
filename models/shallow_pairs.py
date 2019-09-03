Fix computation of shallow-pairs loss

models/shallow_pairs.py
@@ -22,9 +22,9 @@ class ShallowPairEmbedding(EmbeddingModelABC):
             name="card_embedding",
         )
         self.nce_weights = tf.Variable(
-            tf.zeros_initializer()(shape=[embedding_size, n_cards])
+            tf.zeros_initializer()(shape=[n_cards + 1, embedding_size])
         )
-        self.nce_biases = tf.Variable(tf.zeros_initializer()(shape=[n_cards]))
+        self.nce_biases = tf.Variable(tf.zeros_initializer()(shape=[n_cards + 1]))
 
     def compute_output_shape(self, input_shape):
         return tf.TensorShape(input_shape + [self.embedding_size])
@@ -33,29 +33,48 @@ class ShallowPairEmbedding(EmbeddingModelABC):
         return self.card_embedding(card_ids)
 
     def _k_nearest_ids_to_embeddings(self, embeddings, k=10):
-        # Shape: [n_input_embeddings, n_cards]
-        similarities = tf.matmul(
-            embeddings,
-            self.card_embedding.embeddings,
-            transpose_b=True,
-            name="similarity_matrix",
-        )
-        top_similarities, top_indices = tf.math.top_k(similarities, k)
-        return top_indices, top_similarities
+        with tf.name_scope("Nearest_Embeddings"):
+            normalized_input = tf.math.l2_normalize(
+                embeddings, axis=1, name="normalized_input_embeddings"
+            )
+            normalized_embeddings = tf.math.l2_normalize(
+                self.card_embedding.embeddings,
+                axis=1,
+                name="normalized_card_embeddings",
+            )
 
-    def _loss(self, batch):
-        """
-        Compute the loss given a context, target pair.
-        """
-        target, context = batch
-        embeddings = self.call(target)
+            # Shape: [n_input_embeddings, n_cards]
+            similarities = tf.matmul(
+                normalized_input,
+                normalized_embeddings,
+                transpose_b=True,
+                name="similarity_matrix",
+            )
+            top_similarities, top_indices = tf.math.top_k(similarities, k)
+            return top_indices, top_similarities
 
-        return tf.nn.nce_loss(
-            weights=self.prediction_head.weights,
-            biases=self.prediction_head.bias,
-            labels=tf.expand_dims(context, axis=-1),
-            inputs=embeddings,
-            num_sampled=self._n_negative_samples,
-            num_classes=self.n_cards + 1,
-            num_true=1,
-        )
+    def _loss(self, batch, step=None):
+        targets = batch[:, 0]
+        contexts = batch[:, 1]
+
+        embeddings = self.call(targets)
+
+        with tf.name_scope("Loss"):
+            element_loss = tf.nn.nce_loss(
+                weights=self.nce_weights,
+                biases=self.nce_biases,
+                labels=tf.expand_dims(contexts, axis=-1),
+                inputs=embeddings,
+                num_sampled=self._n_negative_samples,
+                num_classes=self.n_cards + 1,
+                num_true=1,
+                name="element_loss",
+            )
+            batch_loss = tf.reduce_mean(element_loss, name="batch_loss")
+
+        if step is not None:
+            tf.summary.histogram("card_embeddings", embeddings, step)
+            tf.summary.histogram("element_losses", element_loss, step)
+            tf.summary.scalar("loss", batch_loss, step)
+
+        return batch_loss